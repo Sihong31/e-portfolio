f1fefd4f8e650c415c78e46efff0e2f09e98c62f
Sihong Huang <sihongspring2011@gmail.com>
2019-09-23 20:53:03 -0400

    add variables, resume link

--- portfolio/src/components/header/header.jsx
+++ portfolio/src/components/header/header.jsx
@@ -23,7 +23,7 @@ class Header extends Component {
                   <NavLink to="/robot" activeClassName="active">Robot as Tutor</NavLink>
                   <NavLink to="/tradewell" activeClassName="active">Tradewell Playbook</NavLink>
                 </NavDropdown>
-                <a href="">Resume</a>
+                <a href="https://drive.google.com/file/d/17JacAQP4abWF6ycw3gKvdamvF8gvWw6I/view" target="_blank">Resume</a>
               </Nav>
             </Navbar.Collapse>
           </Navbar>
