6668e0c26bae1c1fdff46544223655fb0dc6a95f
Sihong Huang <sihongspring2011@gmail.com>
2019-09-23 20:26:46 -0400

    work on project card

--- portfolio/src/components/project-card/project-card.jsx
+++ portfolio/src/components/project-card/project-card.jsx
@@ -3,13 +3,17 @@ import { Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 
 import './project-card.scss';
+import placeholder from '../../assets/images/placeholder.svg';
 
 class ProjectCard extends Component {
   render() {
     return (
       <div className="project-card" style={{ backgroundColor: `${this.props.backgroundColor}` }}>
         <Row>
-          <Col className="content" md={{ offset: 6 }}>
+          <Col className="project-card-image" md={6}>
+            <img src={placeholder} />
+          </Col>
+          <Col className="project-card-content" md={6}>
             <h1>{ this.props.title }</h1>
             <p>{ this.props.description }</p>
             <Link to={ this.props.link }>{ this.props.linkText }</Link>
